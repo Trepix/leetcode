fd10136c7ee0170d7e1deff28529cbf7c6162622
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2020-03-13 11:05:13 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (java)

--- problems/median_of_two_sorted_arrays/solution.java
+++ problems/median_of_two_sorted_arrays/solution.java
@@ -1,31 +1,51 @@
-class Solution {
-public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-int[] arr = new int[nums1.length+nums2.length];
 
-   int i=0;
-    int j=0; 
-    int k=0;
-    
-    while(i<nums1.length && j<nums2.length){
-        
-        if(nums1[i]<nums2[j])
-            arr[k++]=nums1[i++];
-        else
-            arr[k++] = nums2[j++];
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int lastPosition = getFirstValue(nums1, nums2);
+        int secondLastPosition = 0;
+
+        int totalPositions = nums1.length + nums2.length;
+        /*we only need to iterate through the half of the array
+        cause both are sorted.
+        */
+        int maxIterations = ((totalPositions)/2) + 1;
+
+        int i=0;
+        int j=0;
+
+        while(i + j < maxIterations && (i < nums1.length && j < nums2.length)){
+            if(nums1[i]<nums2[j]) {
+                secondLastPosition = lastPosition;
+                lastPosition = nums1[i++];
+            }
+            else {
+                secondLastPosition = lastPosition;
+                lastPosition = nums2[j++];
+            }
         }
-    while(i<nums1.length)
-        arr[k++]=nums1[i++];
-    while(j<nums2.length)
-          arr[k++]=nums2[j++];
-         
-    // arr is the merge sorted array
-     double median = 0;
-          
-     if(arr.length%2==1)
-          median = arr[arr.length/2];
-     else 
-          median = (double)(arr[arr.length/2-1]+arr[arr.length/2])/2;
-          
-      return median;    
-}
+
+        while(i + j < maxIterations && i<nums1.length) {
+            secondLastPosition = lastPosition;
+            lastPosition = nums1[i++];
+        }
+
+        while(i + j < maxIterations && j<nums2.length) {
+            secondLastPosition = lastPosition;
+            lastPosition = nums2[j++];
+        }
+
+        if(totalPositions %2 == 1)
+            return lastPosition;
+        else
+            return (double)(lastPosition + secondLastPosition)/2;
+
+    }
+
+    private int getFirstValue(int[] nums1, int[] nums2) {
+        if (nums1.length == 0) return nums2[0];
+        if (nums2.length == 0) return nums1[0];
+        return Math.min(nums1[0], nums2[0]);
+
+
+    }
 }
