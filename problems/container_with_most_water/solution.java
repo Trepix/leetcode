ffa031fbe12152cfc2da2cfbba6db21ce799c178
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2022-12-30 02:40:32 +0000

    Sync LeetCode submission - Container With Most Water (java)

---
+++ problems/container_with_most_water/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0,l=0,r=height.length-1,ar;
+        while(l<r)
+        {
+            if(height[r]<height[l])
+            {
+                ar=(r-l)*(height[r]);
+                r--;
+            }
+            else
+            {
+                ar=(r-l)*(height[l]);
+                l++;
+            }
+            max=Math.max(max,ar);
+
+        }
+        return max;
+    }
+}
