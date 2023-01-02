b75360c036ce6358b3c7b90e4e8fe0ceecb319e3
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2023-01-02 02:34:24 +0000

    Sync LeetCode submission - 3Sum (java)

--- problems/3sum/solution.java
+++ problems/3sum/solution.java
@@ -1,33 +1,32 @@
-
 import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
+        List<List<Integer>> result = new LinkedList<>();
         Arrays.sort(nums);
-        boolean i_limit_reached = false;
-        for (int i = 0; i < nums.length && !i_limit_reached ; i++) {
-            if (nums[i] > 0) i_limit_reached = true;
-            boolean j_limit_reached = false;
-            int j = i+1, k = nums.length -1;
 
-            while (j < nums.length && !j_limit_reached) {
-                if (j >= k) j_limit_reached = true;
-                else if (nums[i] + nums[j] + nums[k] == 0) result.add(sortedList(nums[i], nums[j], nums[k]));
+        int size = nums.length;
+        if (nums[0] > 0 || nums[size -1] < 0) return new LinkedList<>(result);
 
-                if (Math.abs(nums[i] + nums[j]) < nums[k]) --k;
-                else ++j;
+        for (int i = 0; i < size - 2 && nums[i] <= 0 ; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i+1, right = size -1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) ++left;
+                    while (left < right && nums[right] == nums[right - 1]) --right;
+                    ++left;
+                    --right;
+                }
+                else if (sum > 0) --right;
+                else ++left;
             }
         }
-        return new LinkedList<>(result);
-    }
-
-    static List<Integer> sortedList(int a, int b, int c) {
-        int min = Math.min(a, Math.min(b,c));
-        int max = Math.max(a, Math.max(b,c));
-        int med = a + b + c - min - max;
-        return List.of(min, med, max);
+        return result;
     }
 
 }
