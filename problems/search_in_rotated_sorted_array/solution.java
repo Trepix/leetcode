6545aa34e503768b6c369ea6ddd9c0913f6ca413
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2023-01-14 00:02:53 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- problems/search_in_rotated_sorted_array/solution.java
+++ problems/search_in_rotated_sorted_array/solution.java
@@ -1,8 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for (int i=0; i < nums.length; ++i) {
-            if (nums[i] == target) return i;
+        return search(0, nums.length - 1, nums, target);
+    }
+
+       private int search(int left, int right, int [] nums, int target) {
+        if (left > right) return -1;
+
+        int pivot = (right + left) / 2;
+        if (nums[pivot] == target) return pivot;
+
+        if (isOrdered(left, pivot, nums)) {
+            if (nums[left] <= target && target < nums[pivot]) return search(left, pivot - 1, nums, target);
+            else return search(pivot + 1, right, nums, target);
         }
-        return -1;
+        else {
+            if (nums[pivot] < target && target <= nums[right]) return search(pivot + 1, right, nums, target);
+            else return search(left, pivot - 1, nums, target);
+        }
+    }
+
+    private boolean isOrdered(int left, int right, int[] nums) {
+        return nums[left] <= nums[right];
     }
 }
