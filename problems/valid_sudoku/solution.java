7b214be65e51a5fcd9fffd86e426a6f40215ee1f
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2023-01-17 01:25:35 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- problems/valid_sudoku/solution.java
+++ problems/valid_sudoku/solution.java
@@ -1,5 +1,41 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != '.' && !isValid(board, i, j)) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int i, int j) {
+        char c = board[i][j];
+        for (int k = j + 1; k < board[i].length; k++) {
+            if (board[i][k] == c) {
+                return false;
+            }
+        }
+        for (int k = i + 1; k < board.length; k++) {
+            if (board[k][j] == c) {
+                return false;
+            }
+        }
+
+        int ibox = (i - i % 3);
+        int jbox = (j - j % 3);
+        for (int k = ibox; k < ibox + 3; k++) {
+            for (int l = jbox; l < jbox + 3; l++) {
+                if (k != i && l != j && board[k][l] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+        /*
+    public boolean isValidSudoku(char[][] board) {
 
         for (int i = 0; i < board.length; i++) {
             boolean[] hchars = new boolean[9];
@@ -17,7 +53,7 @@ class Solution {
                     else vchars[c - '1'] = true;
                 }
 
-                c = board[(i / 3) * 3 + (j / 3)][(i * 3) % 9 + (j % 3)];
+                c = board[(i / 3) * 3 + (j / 3)][(i % 3) * 3 + (j % 3)];
                 if (c != '.') {
                     if (schars[c - '1']) return false;
                     else schars[c - '1'] = true;
@@ -26,4 +62,5 @@ class Solution {
         }
         return true;
     }
+    */
 }
